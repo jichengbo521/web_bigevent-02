bbf0962c40d2cc064069fa297fe321f71cf48504
itheima <itheima@itcast.cn>
2021-01-18 15:35:25 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,4 +7,21 @@ var baseURL = 'http://api-breakingnews-web.itheima.net';
 $.ajaxPrefilter(function(options) {
     // 拼接对应的环境地址
     options.url = baseURL + options.url
+        // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //    3.拦截所有响应判断身份认证信息
+    options.complete = function(res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败! ') {
+            //  1.清空本地token
+            localStorage.removeItem("token");
+            // 也秒跳转
+            location.href = "/login.html";
+        }
+    }
 })
